16e00ac179bd6012120d48bf4ce4523fb24b052d
root <yinzhelu@utexas.edu>
2014-10-22 16:28:30 -0500

    Adding new test case for none Integer inputs

--- jenkins-testing/ExperimentTest2.py
+++ jenkins-testing/ExperimentTest2.py
@@ -7,5 +7,8 @@ class MyTestCase(unittest.TestCase):
     r1 = Experiment.largest([3,4,5]) 
     self.assertEqual(r1, 5)
 
+  def test_t2(self):
+    self.assertRaises(ValueError, Experiment.largest, [1.2, 1.1, 1.3])
+
 if __name__ == '__main__':
   unittest.main()
