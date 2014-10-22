a8d32f22b740fbd9a0f17e32ca9c7c5467dcd891
root <yinzhelu@utexas.edu>
2014-10-22 09:56:08 -0500

    Change Experiment.py back to original and add failing testcase

--- jenkins-testing/ExperimentTest.py
+++ jenkins-testing/ExperimentTest.py
@@ -29,6 +29,10 @@ class MyTestCase(unittest.TestCase):
 
   def test_t7(self):
     self.assertRaises(ValueError, Experiment.largest, [])
+  
+  def test_t8(self):
+    r1 = Experiment.largest([-8,-3,-2,-10,-5,-6]) 
+    self.assertEqual(r1, -2)
 
 if __name__ == '__main__':
   unittest.main()
