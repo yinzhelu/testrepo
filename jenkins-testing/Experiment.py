f20f6474e593fbb3c006804bb6280e914e9d0a3e
root <yinzhelu@utexas.edu>
2014-10-22 16:35:05 -0500

    Add check for integer list elements

--- jenkins-testing/Experiment.py
+++ jenkins-testing/Experiment.py
@@ -3,6 +3,9 @@ import sys
 def largest(list):
     if len(list) == 0:
         raise ValueError("Cannot call largest on empty list")
+    for value in list:
+        if type(value) != type(1):
+            raise ValueError("Input value is not an Int!")
     max = -sys.maxint # "smallest" possible int
     #max = 0
     for index in range(len(list)):
@@ -13,6 +16,9 @@ def largest(list):
 def smallest(list):
     if len(list) == 0:
         raise ValueError("Cannot call smallest on empty list")
+    for value in list:
+        if type(value) != type(1):
+            raise ValueError("Input value is not an Int!")
     min = sys.maxint
     for index in range(len(list)):
         if (list[index] < min):
