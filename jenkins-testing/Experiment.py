7a4cc199c94f0d6257b1e4246c80cb2a39614bc0
root <yinzhelu@utexas.edu>
2014-10-21 19:46:03 -0500

    Restore python file

--- jenkins-testing/Experiment.py
+++ jenkins-testing/Experiment.py
@@ -4,7 +4,7 @@ def largest(list):
     if len(list) == 0:
         raise ValueError("Cannot call largest on empty list")
     # max = -sys.maxint # "smallest" possible int
-    max = -10
+    max = -1
     for index in range(len(list)):
         if (list[index] > max):
             max = list[index]
